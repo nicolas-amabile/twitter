77d48e1fc37651b18d766875a8442f56e922ccad
Nicolas Amabile <amabile.nicolas@gmail.com>
2019-11-25 00:55:14 -0300

    New tweet box

--- src/components/NewTweet.jsx
+++ src/components/NewTweet.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addTweet } from '../actions';
+import Avatar from './Avatar';
 
 class NewTweet extends Component {
-  render () {
+  state = { text: '' }
+
+  addTweet() {
+    const { addTweet } = this.props;
+    const { text } = this.state;
+    addTweet(text);
+    this.setState({ text: '' });
+  }
+
+  render() {
+    const { user } = this.props;
     return (
       <div className="new-tweet">
-
+        <Avatar src={user.avatar} />
+        <input className="new-tweet-input" placeholder="What's happening?" onChange={text => this.setState({ text })} />
+        <button className="new-tweet-button" type="button" onClick={this.addTweet}> Tweet </button>
       </div>
     );
   }
 }
 
-export default NewTweet;
+NewTweet.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = ({ user }) => ({
+  user,
+});
+
+export default connect(mapStateToProps, { addTweet })(NewTweet);
