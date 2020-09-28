d61db557949e8e0d2a5aeb0e8f2972ed4afe3e8f
Angelo Dias <oiangelodias@gmail.com>
2020-09-28 16:58:34 -0300

    Fixed ordering of tweets and rewrote TweetsList component as hooks function to solve a few state issues.

--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -1,34 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Tweet from './Tweet';
 
-class TweetsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tweets: props.tweets || [],
-    };
-  }
+const TweetsList = ({ user, contacts, tweets: propTweets }) => {
+  const [tweets, setTweets] = useState([]);
 
-  componentWillReceiveProps(nextProps) {
-    if (this.state.tweets.length !== nextProps.tweets.length) {
-      this.state.tweets = nextProps.tweets;
-    }
-  }
+  useEffect(() => {
+    const tweetComponents = propTweets
+      .sort((a, b) => new Date(b.date) - new Date(a.date))
+      .map(tweet => {
+      // Get the full user object given the tweet.userId
+        const tweetUser = [...contacts, user].find(contact => contact.id === tweet.userId);
+        return <Tweet key={`tweet-${tweet.id}`} {...tweet} user={tweetUser} currentUser={user} />;
+      });
+    setTweets(tweetComponents);
+  }, [contacts, propTweets, user]);
 
-  render() {
-    const { user, contacts } = this.props;
-    return (
-      <div className="tweets-container">
-        {this.state.tweets.map(tweet => {
-          // Get the full user object given the tweet.userId
-          const tweetUser = [...contacts, user].find(contact => contact.id === tweet.userId);
-          return <Tweet key={`tweet-${tweet.id}`} {...tweet} user={tweetUser} currentUser={user} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="tweets-container">
+      {tweets}
+    </div>
+  );
+};
 
 TweetsList.propTypes = {
   contacts: PropTypes.array.isRequired,
