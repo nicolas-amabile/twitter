3c9f87c989983238932b8d42938d07d36dac55e0
Jeremy Then <jeremythen16@gmail.com>
2020-10-27 10:29:23 -0400

    #27 - Adding test cases

--- src/components/NewTweet.test.js
+++ src/components/NewTweet.test.js
@@ -1,16 +1,55 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, within } from '@testing-library/react';
 import { NewTweet } from './NewTweet';
+
+const user = {
+  id: 1,
+  name: 'Jeremy',
+  avatar: { src: '/avatar' }
+}
+
+const addTweet = () => {
+  console.log('Adding Tweet')
+}
 
 describe('New tweet', () => {
   test('it renders correctly', () => {
-    const { container } = render(<NewTweet />);
+    const { container } = render(<NewTweet user={user} addTweet={addTweet} />);
     expect(container).toMatchSnapshot();
   });
-  test.skip('Enforce max lenght for tweet', () => {
-    const { container, getByTestId } = render(<NewTweet />);
-    const input = getByTestId('new-tweet-input');
+  
+  test('Enforce max length for tweet by using the maxChars prop to 30 chars', () => {
+    const { container, getByTestId } = render(<NewTweet maxChars={30} user={user} addTweet={addTweet} />);
+    
+    const input = within(container).getByTestId('new-tweet-input');
     const button = getByTestId('new-tweet-button');
-    // TODO: Try to type more than max number of chars and validate the result
+
+    fireEvent.change(input, { target: {value: 'This is my first tweet with more than 30 chars'}});
+
+    expect(button).toHaveProperty('disabled', true);
+
+    fireEvent.change(input, { target: {value: 'Less than 30 chars'}});
+
+    expect(button).toHaveProperty('disabled', false);
+
   });
+
+  test("Enforce max length for tweet, using default value", () => {
+
+    const { container, getByTestId } = render(<NewTweet user={user} addTweet={addTweet} />);
+    
+    const input = within(container).getByTestId('new-tweet-input');
+
+    const button = getByTestId('new-tweet-button');
+
+    fireEvent.change(input, { target: {value: 'This is my first tweet with more than 60 chars. This is my first tweet with more than 60 chars.'}});
+
+    expect(button).toHaveProperty('disabled', true);
+
+    fireEvent.change(input, { target: {value: 'Less than 60 chars'}});
+
+    expect(button).toHaveProperty('disabled', false);
+
+  });
+
 });
