d911b221b77243796b44063a3f4ca563a299f9f8
Paul Diaz Navarrete <paul.diaz@bairesdev.com>
2020-10-28 11:00:40 -0500

    bugfix/4-fix-follfow-button

--- src/assets/user.js
+++ src/assets/user.js
@@ -1,7 +1,7 @@
 module.exports = {
-  id: '1',
-  avatar: require('./avatars/man-0.png'),
-  name: 'Me',
-  nickname: 'me',
-  contacts: ['02', '05', '08'],
+  id: "1",
+  avatar: require("./avatars/man-0.png"),
+  name: "Me",
+  nickname: "me",
+  contacts: ["2", "5", "8"]
 };
--- src/components/SuggestedContacts.jsx
+++ src/components/SuggestedContacts.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Contact from './Contact';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Contact from "./Contact";
 
-const SuggestedContacts = ({ contacts }) => (
+const SuggestedContacts = ({ contacts, user }) => (
   <div className="suggested-contacts-container">
     <h3>Who to follow</h3>
-    {(contacts || []).map(contact => <Contact key={`contact-${contact.id}`} contact={contact} />)}
+    {(contacts || []).map(contact => {
+      const contactProps = {
+        key: `contact-${contact.id}`,
+        contact,
+        followContact: user.contacts.includes(contact.id)
+      };
+      return <Contact {...contactProps} />;
+    })}
   </div>
 );
 
 SuggestedContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
+  contacts: PropTypes.array.isRequired
 };
 
-const mapStateToProps = ({ contacts }) => ({ contacts });
+const mapStateToProps = ({ contacts, user }) => ({ contacts, user });
 export default connect(mapStateToProps)(SuggestedContacts);
