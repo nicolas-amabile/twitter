6c0cd29113d06ac7a77514e39678fa4488e64ced
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-01-27 16:21:08 -0300

    NewTweet refactor to hooks and fix #13

--- src/components/NewTweet.jsx
+++ src/components/NewTweet.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addTweet as addTweetAction } from '../actions';
@@ -7,47 +7,57 @@ import { isEmpty } from '../utils';
 
 // const MAX_CHARS = 60; // TODO: Implement max for input
 
-export class NewTweet extends Component {
-  state = { text: '' }
+const useTweetPersist = () => {
+  const initialValue = window.localStorage.getItem('pendingTweet');
 
-  publishTweet() {
-    const { user, addTweet } = this.props;
-    const { text } = this.state;
-    addTweet({
-      content: text,
-      userId: user.id,
-      likes: [],
-      date: new Date(),
-      retweets: 0,
-    });
-  }
+  const [text, setText] = useState(initialValue || '');
 
-  render() {
-    const { user } = this.props;
-    if (!user || isEmpty(user)) {
-      return null;
-    }
-    return (
-      <div className="new-tweet">
-        <Avatar src={user.avatar} />
-        <input
-          className="new-tweet-input"
-          placeholder="What's happening?"
-          data-testid="new-tweet-input"
-          onChange={({ target: { value } }) => this.setState({ text: value })}
-        />
-        <button
-          className="new-tweet-button"
-          type="button"
-          data-testid="new-tweet-button"
-          onClick={this.publishTweet}
-        >
-          Tweet
-        </button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    window.localStorage.setItem('pendingTweet', text);
+  }, [text]);
+
+  return [text, setText];
+};
+
+const publishTweet = (user, addTweet, text) => {
+  addTweet({
+    content: text,
+    userId: user.id,
+    likes: [],
+    date: new Date(),
+    retweets: 0,
+  });
+  window.localStorage.setItem('pendingTweet', '');
 }
+
+const NewTweet = ({ user, addTweet }) => {
+  const [text, setText] = useTweetPersist();
+
+  if (!user || isEmpty(user)) {
+    return null;
+  }
+
+  return (
+    <div className="new-tweet">
+      <Avatar src={user.avatar} />
+      <input
+        className="new-tweet-input"
+        placeholder="What's happening?"
+        data-testid="new-tweet-input"
+        value={text}
+        onChange={({ target: { value } }) => setText(value)}
+      />
+      <button
+        className="new-tweet-button"
+        type="button"
+        data-testid="new-tweet-button"
+        onClick={() => publishTweet(user, addTweet, text)}
+      >
+        Tweet
+      </button>
+    </div>
+  );
+};
 
 NewTweet.propTypes = {
   user: PropTypes.object.isRequired,
