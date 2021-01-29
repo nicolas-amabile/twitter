2e926ac1e2ef35142fa8ee4127203fc7a89465b6
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-01-29 18:06:03 -0300

    fix tweets not in order

--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -1,33 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Tweet from './Tweet';
 
-class TweetsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tweets: props.tweets || [],
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.state.tweets.length !== nextProps.tweets.length) {
-      this.state.tweets = nextProps.tweets;
-    }
-  }
-
-  render() {
-    const { user, contacts } = this.props;
-    return (
-      <div className="tweets-container">
-        {this.state.tweets.map(tweet => {
-          // Get the full user object given the tweet.userId
-          const tweetUser = [...contacts, user].find(contact => contact.id === tweet.userId);
-          return <Tweet key={`tweet-${tweet.id}`} {...tweet} user={tweetUser} currentUser={user} />;
-        })}
-      </div>
-    );
-  }
+export default function TweetsList({ contacts, tweets, user }) {
+  return (
+    <div className="tweets-container">
+      {tweets?.sort((a, b) => new Date(b.date) - new Date(a.date)).map(tweet => {
+        // Get the full user object given the tweet.userId
+        const tweetUser = [...contacts, user].find(contact => contact.id === tweet.userId);
+        return <Tweet key={`tweet-${tweet.id}`} {...tweet} user={tweetUser} currentUser={user} />;
+      })}
+    </div>
+  );
 }
 
 TweetsList.propTypes = {
@@ -35,5 +19,3 @@ TweetsList.propTypes = {
   tweets: PropTypes.array.isRequired,
   user: PropTypes.object.isRequired,
 };
-
-export default TweetsList;
