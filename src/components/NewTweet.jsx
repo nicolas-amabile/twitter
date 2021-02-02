13ed585b28e14ea987d2434fb17797ac34eba534
Pablo Tabares H <pablother3@gmail.com>
2021-02-02 12:31:47 -0500

    - Solution for tweet bug - Class to components - uselocalstorage

--- src/components/NewTweet.jsx
+++ src/components/NewTweet.jsx
@@ -4,15 +4,15 @@ import { connect } from 'react-redux';
 import { addTweet as addTweetAction } from '../actions';
 import Avatar from './Avatar';
 import { isEmpty } from '../utils';
+import useLocalStorage from '../utils/useLocalStorage';
 
 // const MAX_CHARS = 60; // TODO: Implement max for input
 
-export class NewTweet extends Component {
-  state = { text: '' }
+export const NewTweet = ({user, addTweet}) => {
+  const [stateLocalStorage, setLocalStorage] = useLocalStorage("tweet");
+  const [text, setText] = React.useState(stateLocalStorage ?? '');
 
-  publishTweet() {
-    const { user, addTweet } = this.props;
-    const { text } = this.state;
+  const publishTweet = () => {
     addTweet({
       content: text,
       userId: user.id,
@@ -20,33 +20,41 @@ export class NewTweet extends Component {
       date: new Date(),
       retweets: 0,
     });
+
+    setText('');
   }
 
-  render() {
-    const { user } = this.props;
-    if (!user || isEmpty(user)) {
-      return null;
-    }
-    return (
-      <div className="new-tweet">
-        <Avatar src={user.avatar} />
-        <input
-          className="new-tweet-input"
-          placeholder="What's happening?"
-          data-testid="new-tweet-input"
-          onChange={({ target: { value } }) => this.setState({ text: value })}
-        />
-        <button
-          className="new-tweet-button"
-          type="button"
-          data-testid="new-tweet-button"
-          onClick={this.publishTweet}
-        >
-          Tweet
-        </button>
-      </div>
-    );
+  const _handleChange = ({ target: { value } }) => {
+    setText(value);
+    
+    //TODO: Add Debounce lodash here to improve performance
+    setLocalStorage(value)
   }
+
+  if (!user || isEmpty(user)) {
+    return null;
+  }
+
+  return (
+    <div className="new-tweet">
+      <Avatar src={user.avatar} />
+      <input
+        className="new-tweet-input"
+        placeholder="What's happening?"
+        data-testid="new-tweet-input"
+        value={text}
+        onChange={_handleChange}
+      />
+      <button
+        className="new-tweet-button"
+        type="button"
+        data-testid="new-tweet-button"
+        onClick={publishTweet}
+      >
+        Tweet
+      </button>
+    </div>
+  );
 }
 
 NewTweet.propTypes = {
